794197b2ee53a010b9a76a964bc22274069ca194
Enzo Mescall <emm121@duke.edu>
2024-02-03 00:24:13 -0500

    what the FUCK

--- heatmap_maker.py
+++ heatmap_maker.py
@@ -2,7 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 from matplotlib.animation import FuncAnimation
-from fplanck import fokker_planck, boundary, gaussian_pdf
+from fplanck import fokker_planck, boundary, delta_function, potential_from_data
+from mpl_toolkits.mplot3d import Axes3D
 
 """
 Solve the Fokker-Planck equation
@@ -17,49 +18,68 @@ Arguments:
     boundary        type of boundary condition (scalar or vector, default: reflecting)
 
 Translation:
-drift/mu -> force
-sigma/diffusion -> drag (or 1/drag)
+drift/mu -> potential
+sigma/diffusion -> drag (or 1/drag unsure)
 """
 
-initial_values = (0, 0, 0)
+nm = 1e-9
+viscosity = 8e-4
+radius = 50*nm
+drag = 6*np.pi*viscosity*radius*20
 
-def mu(x, y, z):
-    return (x, y, z)    
+"""
+Arguments:
+    grid     list of grid arrays along each dimension
+    data     potential data
+"""
 
-def sigma(x, y, z):
-    return (x, y, z)
+# Generate a grid of points in 2d space
+x = np.linspace(-1e-19, 1e-19, 200)
+y = np.linspace(-1e-19, 1e-19, 200)
 
-sim = fokker_planck(temperature=10,
-                    drag=sigma,
-                    extent=200,
-                    resolution=1,
-                    force=mu,
-                    boundary=boundary.absorbing)
+# Create a 2D vector field on these points
+gx, gy = np.meshgrid(x, y)
 
-### steady-state solution
-steady = sim.steady_state()
+def vector_field_2d(gx, gy):
+    return  gx - gy
+
+grid = np.array([x, y])
+data = vector_field_2d(gx, gy)
+
+
+
+U = potential_from_data(grid, data) 
+
+sim = fokker_planck(temperature=300,
+                    drag=drag,
+                    extent=[600*nm, 600*nm],
+                    resolution=10*nm,
+                    boundary=boundary.reflecting,
+                    potential=U)
 
 ### time-evolved solution
-w = 30
-pdf = gaussian_pdf(0, w)
-p0 = pdf(sim.grid[0])
+pdf = delta_function((-150*nm, -150*nm))
+p0 = pdf(*sim.grid)
 
 Nsteps = 200
-time, Pt = sim.propagate_interval(pdf, 1e-3, Nsteps=Nsteps)
+time, Pt = sim.propagate_interval(pdf, 2e-3, Nsteps=Nsteps)
 
 ### animation
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(subplot_kw=dict(projection='3d'), constrained_layout=True)
 
-ax.plot(sim.grid[0], steady, color='k', ls='--', alpha=.5)
-ax.plot(sim.grid[0], p0, color='red', ls='--', alpha=.3)
-line, = ax.plot(sim.grid[0], p0, lw=2, color='C3')
+surf = ax.plot_surface(*sim.grid/nm, p0, cmap='viridis')
+
+ax.set_zlim([0,np.max(Pt)/3])
+ax.autoscale(False)
 
 def update(i):
-    line.set_ydata(Pt[i])
-    return [line]
+    global surf
+    surf.remove()
+    surf = ax.plot_surface(*sim.grid/nm, Pt[i], cmap='viridis')
+
+    return [surf]
 
 anim = FuncAnimation(fig, update, frames=range(Nsteps), interval=30)
-ax.set(xlabel='x', ylabel='normalized PDF')
-ax.margins(x=0)
+ax.set(xlabel='x (nm)', ylabel='y (nm)', zlabel='normalized PDF')
 
 plt.show()
--- nproll_test.py
+++ nproll_test.py
@@ -12,4 +12,4 @@ def force(a,b,c):
 F = np.atleast_2d(force(1,2,3))
 
 for i in range(extent.size):
-    print(np.roll(F[i], -1, axis=i))
+    print(np.roll(F[i], -1, axis=i))    
